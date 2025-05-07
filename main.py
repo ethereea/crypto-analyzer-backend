a5463c62029e3d7178e348576c4f4aff9db80fda
Virgil Pascu <virgil.pascu@Virgils-MacBook-Pro.local>
2025-05-07 13:48:12 +0300

    Enable CORS to allow frontend access

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from textblob import TextBlob
+from fastapi.middleware.cors import CORSMiddleware
 import random
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # sau poți pune domeniul tău specific
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     text: str
